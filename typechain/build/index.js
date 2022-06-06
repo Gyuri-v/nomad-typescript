f4b16ddf3d5bc8feece05a3e31126347a0ccb42d
Gyuri-v <fnffkdll@naver.com>
2022-06-06 23:37:49 +0900

    dict 사전 만들어보기

--- typechain/build/index.js
+++ typechain/build/index.js
@@ -1,1 +1,48 @@
-var hello = function () { return "hi"; };
+class Dict {
+    constructor() {
+        this.words = {};
+    }
+    add(word) {
+        if (this.words[word.term] === undefined) { // 주어진 단어가 아직 사전에 존재하지 않으면
+            this.words[word.term] = word.def;
+        }
+    }
+    def(term) {
+        return this.words[term];
+    }
+    update(word) {
+        if (this.words[word.term] !== undefined) {
+            this.words[word.term] = word.def;
+        }
+    }
+    del(term) {
+        if (this.words[term] !== undefined) {
+            delete this.words[term];
+        }
+    }
+    // static method - js
+    static hello() {
+        return "hello";
+    }
+}
+class Word {
+    constructor(
+    // public 이지만 변경불가능하게 하려면 readonly
+    term, def) {
+        this.term = term;
+        this.def = def;
+    }
+}
+const kimchi = new Word("kimchi", "한국의 음식");
+const pizza = new Word("banana", "원숭이는 바나나를 좋아해");
+const dict = new Dict();
+dict.add(kimchi);
+dict.add(pizza);
+console.log(dict.def("kimchi"));
+console.log(dict.def("banana"));
+dict.update(new Word("kimchi", "한국인이 좋아하는 음식"));
+console.log(dict.def("kimchi"));
+console.log(dict.def("banana"));
+dict.del("banana");
+console.log(dict.def("pizza"));
+console.log(dict.def("banana"));
--- typechain/src/index.ts
+++ typechain/src/index.ts
@@ -1,1 +1,58 @@
-const hello = () => "hi";
+type Words = {
+  [key:string]: string
+}
+
+class Dict {
+  private words: Words
+  constructor(){
+      this.words = {}
+  }
+  add(word:Word){
+      if(this.words[word.term] === undefined){ // 주어진 단어가 아직 사전에 존재하지 않으면
+          this.words[word.term] = word.def;
+      }
+  }
+  def(term:string){
+      return this.words[term]
+  }
+  update(word: Word) {
+      if (this.words[word.term] !== undefined) {
+          this.words[word.term] = word.def;
+      }
+  }
+  del(term: string) {
+      if (this.words[term] !== undefined) {
+          delete this.words[term];
+      }
+  }
+  // static method - js
+  static hello() {
+      return "hello"
+  }
+}
+
+class Word {
+  constructor(
+      // public 이지만 변경불가능하게 하려면 readonly
+      public readonly term:string,
+      public readonly def:string,
+  ){}
+}
+
+const kimchi = new Word("kimchi", "한국의 음식");
+const pizza = new Word("banana", "원숭이는 바나나를 좋아해");
+
+const dict = new Dict();
+
+dict.add(kimchi);
+dict.add(pizza);
+console.log(dict.def("kimchi"));
+console.log(dict.def("banana"));
+
+dict.update(new Word("kimchi", "한국인이 좋아하는 음식"));
+console.log(dict.def("kimchi"));
+console.log(dict.def("banana"));
+
+dict.del("banana");
+console.log(dict.def("pizza"));
+console.log(dict.def("banana"));
