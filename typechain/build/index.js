6db467bd32564a5a391a6274fe3430a4f999a8a2
Gyuri-v <fnffkdll@naver.com>
2022-06-09 23:48:08 +0900

    prettier 추가 / Targets 강의

--- typechain/build/index.js
+++ typechain/build/index.js
@@ -1,35 +1,8 @@
-class Player {
-    // extends 대신 implements 을 사용하면 js 에는 interface는 사라지는데, 코드가 가벼워짐
-    // typescript가 위의 해당 property 를 가지도록 강제시켜줌
-    constructor(
-    // interface에서 설정한 property 들은 public 이어야만 함
-    firstName, lastName, health) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.health = health;
+class Block {
+    constructor(data) {
+        this.data = data;
     }
-    fullName() {
-        return `${this.firstName} ${this.lastName}`;
-    }
-    sayHi(name) {
-        return `Hello ${name}. I'm ${this.firstName}`;
+    static hello() {
+        return "hi";
     }
 }
-// 함수에서 interface를 타입으로 쓸 수 있고, return 할 수도 있음
-// function makeUser(user: User){
-//   return "hi"
-// }
-function makeUser(user) {
-    return {
-        firstName: "nico",
-        lastName: "las",
-        fullName: () => "xx",
-        sayHi: (name) => "string"
-    };
-}
-makeUser({
-    firstName: "nico",
-    lastName: "las",
-    fullName: () => "xx",
-    sayHi: (name) => "string"
-});
--- typechain/src/index.ts
+++ typechain/src/index.ts
@@ -1,46 +1,6 @@
-interface User {
-  firstName: string,
-  lastName: string,
-  sayHi(name:string): string,
-  fullName(): string,
-}
-// 두개의 interface를 동시에 상속할 수 있음
-interface Human {
-  health: number,
-}
-class Player implements User, Human{
-  // extends 대신 implements 을 사용하면 js 에는 interface는 사라지는데, 코드가 가벼워짐
-  // typescript가 위의 해당 property 를 가지도록 강제시켜줌
-  constructor(
-      // interface에서 설정한 property 들은 public 이어야만 함
-      public firstName: string,
-      public lastName: string,
-      public health: number,
-  ){}
-  fullName() {
-      return `${this.firstName} ${this.lastName}`
-  }
-  sayHi(name:string) {
-      return `Hello ${name}. I'm ${this.firstName}`
+class Block {
+  constructor(private data:string){}
+  static hello() {
+    return "hi"
   }
 }
-
-// 함수에서 interface를 타입으로 쓸 수 있고, return 할 수도 있음
-// function makeUser(user: User){
-//   return "hi"
-// }
-function makeUser(user: User): User{
-  return {
-    firstName: "nico",
-    lastName: "las",
-    fullName: ()=> "xx",
-    sayHi: (name)=> "string"
-  }
-}
-
-makeUser({
-  firstName: "nico",
-  lastName: "las",
-  fullName: ()=> "xx",
-  sayHi: (name)=> "string"
-})
