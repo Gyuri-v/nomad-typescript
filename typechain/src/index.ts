e33a880ce16336408db3db6d015c95b7dc7b23da
Gyuri-v <fnffkdll@naver.com>
2022-06-13 23:22:12 +0900

    declaration files

--- typechain/src/index.ts
+++ typechain/src/index.ts
@@ -1,27 +1,7 @@
-interface SStorage<T> {
-  [key: string]: T
-}
+import {init, exit} from "myPackage";
 
-class LocalStorage<T> {
-  private storage : SStorage<T> = {}
-  set(key: string, value: T){
-    this.storage[key] = value
-  }
-  remove(key: string){
-    delete this.storage[key]
-  }
-  get(key:string):T {
-    return this.storage[key]
-  }
-  clear(){
-    this.storage = {}
-  }
-}
+init({
+  url: "true"
+})
 
-const stringsStorage = new LocalStorage<string>()
-stringsStorage.get("key");
-stringsStorage.set("hello", "how are you")
-
-const booleanStorage = new LocalStorage<boolean>();
-booleanStorage.get("xxx");
-booleanStorage.set("hello", true);
+exit(1)
